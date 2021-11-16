b652880114278e35ef3354294b0a1e66850d36bf
Mehdirps <mehdi.raposo77@gmail.com>
2021-11-16 11:58:51 +0100

    Pause midi, essaie avec modal

--- scripts.js
+++ scripts.js
@@ -1,20 +1,72 @@
-let smallImages = document.querySelectorAll(".small-img");
-let images = document.querySelectorAll(".img");
+// let smallImages = document.querySelectorAll(".small-img");
+// let images = document.querySelectorAll(".img");
+let images = document.querySelectorAll("a");
 let fond = document.querySelector(".fond");
 let close = document.querySelector(".close");
+let modal = document.querySelector(".modal");
+let modalImg = document.querySelector(".modal img");
+// for (let image of smallImages) {
 
-for (let image of smallImages) {
+//     image.addEventListener("click", () => {
 
-    image.addEventListener("click", () => {
+//         fond.style.display = "block";
 
-        fond.style.display = "block";
+//         image.classList.add("show");
+
+//         close.style.display = "initial";
+
+//     });
+
+//     fond.addEventListener("click", () => {
+
+//         fond.style.display = "none";
+
+//         image.classList.remove("show");
+
+//         close.style.display = "none";
+//     });
+
+//     close.addEventListener("click", () => {
+
+//         fond.style.display = "none";
+
+//         image.classList.remove("show");
+
+//         close.style.display = "none";
+
+//     });
+
+//     for(img of images){
+
+//         img.addEventListener("click", () => {
+
+//             fond.style.display = "none";
+
+//             image.classList.remove("show");
+
+//             close.style.display = "none";
+//         });
+//     }
+// }
+for (let image of images) {
+
+    image.addEventListener("click", (e) => {
+        
+        e.preventDefault();
+        
+        let link = image.href;
+        
+        modal.classList.add("show");
+        
+        modalImg.src = link;
 
         image.classList.add("show");
-
+        
+        fond.style.display = "block";
+        
         close.style.display = "initial";
-
-    });
-
+        
+    })
     fond.addEventListener("click", () => {
 
         fond.style.display = "none";
@@ -23,25 +75,25 @@ for (let image of smallImages) {
 
         close.style.display = "none";
     });
-    
+
     close.addEventListener("click", () => {
 
         fond.style.display = "none";
-    
+
         image.classList.remove("show");
-    
+
         close.style.display = "none";
-        
+
     });
-    
-    for(img of images){
+
+    for (img of images) {
 
         img.addEventListener("click", () => {
-    
+
             fond.style.display = "none";
-    
+
             image.classList.remove("show");
-    
+
             close.style.display = "none";
         });
     }
